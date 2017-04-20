e75925f304a2cf8e4e8471271e90fb0b79d16b59
Jeroen <grootenjeroen@hotmail.com>
2017-04-20 09:55:38 +0200

    Added non-functional slide boxes to Home

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,6 +1,20 @@
-<div class="nieuw_film_slider">
-  test
-</div>
+<div class="top-home">
+  <div class="slider">
 
+  </div>
+  <div class="slide-boxes">
+    <div class="slide-box">
+      1
+    </div>
+    <div class="slide-box">
+      2
+    </div>
+    <div class="slide-box">
+      3
+    </div>
+  </div>
+</div>
+<hr>
+test
 <?php
 DB::conn()->close();
