d3e5cf379078f6f09a799a297d37a56b182d398f
Jeroen <grootenjeroen@hotmail.com>
2017-05-04 13:13:18 +0200

    Reformated some code in Afrekenen

--- resources/views/afrekenen.php
+++ resources/views/afrekenen.php
@@ -3,10 +3,79 @@ $afrekenen = new Afrekenen;
 $account = new Account;
 $login = new winkelmandLogin;
 $registreer = new winkelmandRegistreer;
+$winkelmand = new Winkelmand;
+$artikel = new Artikel;
 
 if(!empty($_POST)){
 
-  if(!$account->isIngelogd()){
+  if($account->isIngelogd()){
+
+    switch($_GET['stap']){
+      case 'opmerking':
+      $afrekenen->slaBezorgwijzeOpInSession($_POST['bezorgwijze']);
+      if($afrekenen->controlleerBezorgwijzeSession()){
+        ?>
+        <h1>AFREKENEN</h1>
+        <hr>
+        <h3>OPMERKING</h3>
+
+        <form method="post" action="?stap=overzicht">
+          <textarea name="opmerking" placeholder="OPMERKING (optioneel)" class="form-control"></textarea>
+          <button class="btn btn-primary form-knop">VERDER</button>
+        </form>
+        <?php
+      }else{
+        echo '<div class="warning"><b>Er is een fout opgetreden. Probeer het later opnieuw</b></div>';
+      }
+      break;
+
+      case 'overzicht':
+      if(!empty($_POST['opmerking'])){
+        $afrekenen->storeOpmerkingInSession($_POST['opmerking']);
+      }
+      $gebruikerId = $account->getLoginId();
+      $artikelen = $winkelmand->getArtikelen($gebruikerId);
+
+      if(!empty($artikelen)){
+        ?>
+        <h1>AFREKENEN</h1>
+        <hr>
+        <h4>OVERZICHT</h4>
+        <p><b>TODO: PRINT KNOP, BESTELLING AFRONDEN EN OPTIE VOOR ANULERING ORDER<b></p>
+          <table class="table winkelmand_table">
+            <tbody>
+              <?php
+              $totaal = array();
+              foreach ($artikelen as $artikelId){
+                $info = $artikel->thumbInfo($artikelId);
+                $totaal[] = $info['prijs'];
+                ?>
+                <tr>
+                  <td><a href="/artikel/<?php echo $artikelId?>"><img src="/foto/<?php echo $info['img']?>" class="winkelmand_img"></a></td>
+                  <td><p><?php echo $info['titel'] ?></p></td>
+                  <td><h4>€<?php echo $info['prijs'] ?></h4></td>
+                  <td>
+                    <form method="post" action="?action=delete&code=<?php echo $artikelId ?>">
+                      <button type="submit" class="btn btn-success">
+                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                      </button>
+                    </form>
+                  </td>
+                </tr>
+                <?php
+              }
+              ?>
+            </tbody>
+          </table>
+          <?php
+          echo '<h4><b>TOTAAL PRIJS: €'.array_sum($totaal).'</b></h4>';
+          ?>
+          <a href="/?afgerekend=true"><button class="btn btn-succes form-knop">AFREKENEN</button></a>
+          <?php
+        }
+        break;
+      }
+  }else{
     if($_POST['submit'] == 'LOGIN'){
       $login->login($_SESSION['winkelmand'], $_POST['email'], $_POST['wachtwoord']);
     }elseif($_POST['submit'] == 'REGISTREER'){
@@ -14,33 +83,6 @@ if(!empty($_POST)){
     }elseif($_GET['bestellen'] == 'anoniem'){
       echo '<u><b>TODO:</b> Anoniem bestellen</u>';
     }
-  }else{
-    switch($_GET['stap']){
-      case 'opmerking':
-        $afrekenen->slaBezorgwijzeOpInSession($_POST['bezorgwijze']);
-        if($afrekenen->controlleerBezorgwijzeSession()){
-          ?>
-          <h1>AFREKENEN</h1>
-          <hr>
-          <h3>OPMERKING</h3>
-
-          <form method="post" action="?stap=overzicht">
-          <textarea name="opmerking" placeholder="OPMERKING (optioneel)" class="form-control"></textarea>
-          <button class="btn btn-primary form-knop">VERDER</button>
-          </form>
-          <?php
-        }else{
-          echo '<div class="warning"><b>Er is een fout opgetreden. Probeer het later opnieuw</b></div>';
-        }
-        break;
-
-      case 'overzicht':
-        if(!empty($_POST['opmerking'])){
-          $afrekenen->storeOpmerkingInSession($_POST['opmerking']);
-        }
-        break;
-    }
-
   }
 
 }else{
