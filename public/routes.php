f87aa9b1e2ea96729b3a93e63e041de0178d7b2c
Jeroen <grootenjeroen@hotmail.com>
2017-05-08 12:43:37 +0200

    Removed the Beheer Overzicht page

--- public/routes.php
+++ public/routes.php
@@ -56,10 +56,6 @@ $klein->respond('/beheer/orchidee_toevoegen', function ($request, $response, $se
     $service->pageTitle = 'Orchidee toevoegen | Orchidee Holiday';
     $service->render(VIEWS.'/orchidee_toevoegen.php');
 });
-$klein->respond('/beheer/overzicht', function ($request, $response, $service) {
-    $service->pageTitle = 'Overzicht | Orchidee Holiday';
-    $service->render(VIEWS.'/overzicht.php');
-});
 $klein->respond('/artikel/[:id]', function ($request, $response, $service) {
     $service->orchideeId = $request->id;
     $service->pageTitle = 'Artikel | Orchidee Holiday';
