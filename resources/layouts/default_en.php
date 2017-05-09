a972a45461acfeabc02b677c1f1cf9267dd7c273
Jeroen <grootenjeroen@hotmail.com>
2017-05-09 09:00:06 +0200

    Changed menu item in the translated versio to fit the screen

--- resources/layouts/default_en.php
+++ resources/layouts/default_en.php
@@ -83,7 +83,7 @@ if(!empty($_SESSION['login'])){
         </div>
         <div class="navbar-collapse collapse navbar-responsive-collapse">
           <ul class="nav navbar-nav nav-links">
-            <li><a href="/aanbod">PRODUCT OVERVIEW</a></li>
+            <li><a href="/aanbod">SHOP OFFER</a></li>
             <li><a href="/artikel_groepen">PRODUCT GROUPS</a></li>
             <li><a href="/best_verkocht">BEST SOLD</a></li>
             <li><a href="/contact">CONTACT</a></li>
