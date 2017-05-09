f2f6cf8aa013b8c1e2de7d38ed0a66c06703c642
Jeroen <grootenjeroen@hotmail.com>
2017-05-09 16:00:38 +0200

    Added explainatory comments to the Afrekenen class functions

--- resources/library/afrekenen.php
+++ resources/library/afrekenen.php
@@ -1,29 +1,46 @@
 <?php
 class Afrekenen{
+
+  /*
+  Store the selected Bezorgwijze in the 'bezorgwijze' session
+
+  @param: The user's selected Bezorgwijze
+  */
   public function slaBezorgwijzeOpInSession($bezorgwijze){
     $_SESSION['bezorgwijze'] = array();
     $_SESSION['bezorgwijze'] = $bezorgwijze;
   }
 
+  /*
+  Check if the 'bezorgwijze' session is empty or not.
+
+  @return: True, if it is not empty
+  */
   public function controlleerBezorgwijzeSession(){
-    if(empty($_SESSION['bezorgwijze'])){
-      return false;
-    }else{
+    if(!empty($_SESSION['bezorgwijze'])){
       return true;
     }
   }
 
+  /*
+  Store the user's Opmerking in the 'opmerking' session
+
+  @param: The user's Opmerking
+  */
   public function storeOpmerkingInSession($opmerking){
     $_SESSION['opmerking'] = array();
     $_SESSION['opmerking'] = $opmerking;
   }
 
+  /*
+  Get the user's Opmerking
+
+  @return: The user's Operking
+  */
   public function getSessionOpmerking(){
     if(!empty($_SESSION['opmerking'])){
       $opmerking = $_SESSION['opmerking'];
       return $opmerking;
-    }else{
-      return false;
     }
   }
 
