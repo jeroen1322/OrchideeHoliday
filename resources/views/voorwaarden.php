74baa76ec8803fffce6a6995ac86a73edf82939c
Jeroen <grootenjeroen@hotmail.com>
2017-05-22 20:14:35 +0200

    Changed the terms of service to be less intrusive and respect the user

--- resources/views/voorwaarden.php
+++ resources/views/voorwaarden.php
@@ -3,7 +3,7 @@
   <a href="/voorwaarden/en"><img src="/foto/english_flag.png" class="flag"></a>
 </div>
 <h1>Voorwaarden</h1>
-<p>Bedankt voor het bezoeken van onze website. Door het bezoeken van deze website gaat u akkoord met deze website voorwaarden. Lees deze voorwaarden daarom aandachtig door. Wij behouden ons het recht voor, zonder voorafgaande kennisgeving, wijzigingen in deze voorwaarden aan te brengen.</p>
+<p>Bedankt voor het bezoeken van onze website. Door het bezoeken van deze website gaat u akkoord met deze website voorwaarden. Lees deze voorwaarden daarom aandachtig door.</p>
 
 <h2>Aansprakelijkheid</h2>
 <p>Wij hebben deze website met zorg samengesteld om er voor te zorgen dat de inhoud van de website correct en compleet is. De website wordt echter alleen voor informatieve doeleinden aangeboden en u bezoekt deze website voor eigen risico. Wij garanderen niet dat de website of de servers waarop deze website staat, vrij zijn van virussen.
@@ -12,8 +12,3 @@ Voor zover wettelijk toegestaan, sluiten wij iedere aansprakelijkheid uit voor w
 
 <h2>Links</h2>
 <p>Deze website kan links of andere verwijzingen bevatten naar andere websites. Dit betekent niet dat wij verbonden zijn met deze andere websites en verantwoordelijk zijn voor deze andere websites inclusief de daarin opgenomen informatie.</p>
-
-<h2>Informatie die u aan ons verstrekt</h2>
-<p>Alle informatie die u aan ons verstrekt via deze website, zoals materiaal, data, vragen, commentaar, of suggesties zijn niet-vertrouwelijk en zullen als zodanig worden behandeld. Informatie die u aan ons verstrekt, wordt eigendom van OrchideeHoliday en kan gebruikt worden door OrchideeHoliday voor bijvoorbeeld reproductie, online publicatie of print publicatie. Bovendien is het ons toegestaan om ideeën, drukwerk, uitvindingen, suggesties of concepten die onderdeel zijn van de informatie die u ons verstrekt via deze website, te gebruiken zonder dat wij hiervoor een vergoeding aan u verschuldigd zijn.
-<br><br>
-Indien u aan ons informatie verstrekt, garandeert u dat u de eigenaar bent van deze informatie en dat het gebruik door OrchideeHoliday van deze informatie geen inbreuk maakt op de (intellectuele) eigendomsrechten van derden of anderszins onrechtmatig is. OrchideeHoliday heeft nooit de verplichting om enige informatie die u verstrekt, te gebruiken. </p>
--- resources/views/voorwaarden_en.php
+++ resources/views/voorwaarden_en.php
@@ -4,7 +4,6 @@
 </div>
 <h1>Terms and Conditions</h1>
 <p>Thank you for using our website. By visiting this website, you agree with the website's terms and conditions. Please read the terms and condition carefully.
-We have the right to change the terms and conditions, without prior knowlegde of the user.</p>
 
 <h2>Liability</h2>
 <p>We carefully developed this website so that all the content and information presented on this website is correct and complete. However, the website is only provided for informative purposes and users use this website at their own risk. We do not guarantee that this website, or the server on which this website is hosted, is virus free.
@@ -13,9 +12,3 @@ As far as lawfully allowed, we are not liable for any damage what so ever, direc
 
 <h2>Links</h2>
 <p>This website may contain references to other website. This do not mean that we are related to or responsible for these websites, including the information presented on these websites.</p>
-
-<h2>Information that you provide to us</h2>
-<p>All the information that you provide to use through usage of this website, like material, data, questions, comments or suggestions are not classified and will be treated as public information. Information that you provide to us, will become property of Orchidee Holiday and can be used by OrchideeHoliday commercial purposes like online or print publication.
-  We are also allowed to use the ideas, prints, inventions, suggenstions or concepts that are part of the information that you provide to us by using this website, without your permission and we are not obligated to pay you.
-<br><br>
-If you do provide us with information, you garantee that you are the owner of this information and that the usage of this information by OrchideeHoliday will not result in infringin on the (intelectual) property of third parties, which is illegal. OrchideeHoliday is never required to use that information that you provide us with.</p>
