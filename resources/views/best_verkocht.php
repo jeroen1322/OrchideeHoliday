6e3a5bdd69016798b69b5ed1ff577d4bc1ed2999
Jeroen <grootenjeroen@hotmail.com>
2017-06-10 14:43:34 +0200

    Removed some commented-out code

--- resources/views/best_verkocht.php
+++ resources/views/best_verkocht.php
@@ -9,7 +9,6 @@ if(!empty($_GET)){
   if($_GET['action'] == 'bestel'){
     if($gebruiker->isIngelogd()){
       $winkelmand->plaatsInDatabaseWinkelmand($_GET['id'], $_SESSION['login'][0], 'best_verkocht');
-      // header("Refresh:0; url=/best_verkocht");
     }else{
       $winkelmand->plaatsInSessionWinkelmand($_GET['id'], 'best_verkocht');
       header("Refresh:0; url=/best_verkocht");
